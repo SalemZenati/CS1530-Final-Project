ad8a44c960f0f23ae882dddb5fbed1bf1d61c301
BMR81 <BMR81@pitt.edu>
2024-04-25 17:36:15 -0400

    Handling Negative Values in Budget Class

--- financeApp/src/main/java/com/example/Budget.java
+++ financeApp/src/main/java/com/example/Budget.java
@@ -1,59 +1,91 @@
 package com.example;
 
 public class Budget {
-    
+
+    // Constant to keep track of the default value for a budget
+    private final double defaultValue = 0.0;
+    // Other fields of a Budget Object
     private int bid;
     private String name;
     private double budgetAmount;
     private double budgetSpent;
     private double budgetLeft;
 
+    // Constructor for Budget Object
     public Budget(int bid, String name, double budgetAmount, double budgetSpent){
         this.bid = bid;
         this.name = name;
+
+        // Set budgetAmount to be the requested amount then check if it is valid
         this.budgetAmount = budgetAmount;
+        // If the budgetAmount is negative, set it to the default value
+        if (budgetAmount < 0) this.budgetAmount = defaultValue;
+
+        // Set budgetSpent (the amount of money spent in a budget) to the requested amount
         this.budgetSpent = budgetSpent;
-        this.budgetLeft = (budgetAmount - budgetSpent);
+        // Check if the requested amount was negative, set it to the default value if so
+        if (budgetSpent < 0) this.budgetSpent = defaultValue;
+
+        // Automatically calculate how much a user has left to spend in a budget using the total amount and amount spent
+        this.budgetLeft = (this.budgetAmount - this.budgetSpent);
     }
 
-    public int getBid(){
-        return this.bid;
-    }
-
+    // Method to change the name field of a Budget Object
     public void updateName(String name){
         this.name = name;
     }
 
+    // Method to change the budgetAmount field of a Budget Object
     public void updateBudgetAmount(double budgetAmount){
         this.budgetAmount = budgetAmount;
+        // If the new amount requested is negative, set it to the default value
+        if(budgetAmount < 0) this.budgetAmount = defaultValue;
+
+        // Automatically recalculate the amount left in a budget and call the helper method to do so
         this.calculateBudgetLeft();
     }
 
+    // Method to change the budgetSpent field of a Budget Object
     public void updateBudgetSpent(double budgetSpent){
         this.budgetSpent = budgetSpent;
+        // If the new amount requested is negative, set it to the default value
+        if(budgetSpent < 0) this.budgetSpent = defaultValue;
+
+        // Automatically recalculate the amount left in a budget and call the helper method to do so
         this.calculateBudgetLeft();
     }
     
+    // Helper method to recalculate the amount left to spend in a budget and update the budgetLeft field
     public void calculateBudgetLeft(){
         this.budgetLeft = (this.budgetAmount - this.budgetSpent);
     }
 
+    // Getter for the bid field
+    public int getBid(){
+        return this.bid;
+    }
+
+    // Getter for the name field
     public String getName(){
         return this.name;
     }
 
+    // Getter for the budgetAmount field
     public double getBudgetAmount(){
         return this.budgetAmount;
     }
 
+    // Getter for the budgetSpent field
     public double getBudgetSpent(){
         return this.budgetSpent;
     }
 
+    // Getter for the budgetLeft field
     public double getBudgetLeft(){
         return this.budgetLeft;
     }
 
+    // toString() implementation for a BudgetObject
     @Override
     public String toString() {
         return String.format("%s - Goal: $%.2f, Spent: $%.2f, Remaining: $%.2f", name, budgetAmount, budgetSpent, budgetLeft);
